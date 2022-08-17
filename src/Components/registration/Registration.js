6a123108f190fa55397075997a6b23c8e22a901d
tezzv <106775682+tezzv@users.noreply.github.com>
2022-08-18 00:06:34 +0530

    registrationresponsive

--- src/Components/registration/Registration.js
+++ src/Components/registration/Registration.js
@@ -1,29 +1,28 @@
-import React,  { useState } from 'react'
+import React, { useState } from 'react'
 import './Registration.css';
-import { useNavigate, Link} from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 
 function Registration() {
 
   let navigate = useNavigate();
 
-  
-  
+
+
 
   return (
     <div className='registrationPage'>
-    
-    <Link  to="/"><img className='sugaLogoBW' src='/images/suga logo bw.svg'/></Link>
-    <div><img className='sugaText' src='/images/SuGa text.png'/></div>
-    <button className='back' onClick={() => navigate(-1)}>Back</button>
-    
-    <span className='enterEmailText'>Enter Email </span>
-    <input type="text" id="userEmail" className='enterEmail' ></input> <br />
-      
-  
-  <button className='sendInvite' id='sendInvite'>Send Invite</button>
+      <div className='registrationTop1'>
+        <div><Link to="/"><img className='sugaLogoBW' src='/images/suga logo bw.svg' /></Link></div>
+        <div><img className='sugaText' src='/images/SuGa text.png' /></div>
+        <div className='back1'><button className='back' onClick={() => navigate(-1)}>Back</button></div>
+      </div>
+      <div className='flexregistration1'>
+        <span className='enterEmailText'>Enter Email </span>
+        <input type="text" id="userEmail" className='enterEmail' ></input> <br />
+        <button className='sendInvite' id='sendInvite'>Send Invite</button>
+      </div>
     </div>
-    
   )
 }
 
