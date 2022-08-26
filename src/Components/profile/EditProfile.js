e7deef03218c43110c506440ba44df240426a3aa
tezzv <106775682+tezzv@users.noreply.github.com>
2022-08-27 02:29:16 +0530

    edit profile3

--- src/Components/profile/EditProfile.js
+++ src/Components/profile/EditProfile.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './EditProfile.css'
 import Navbar from '../navbar/Navbar'
-import cancelLogo from './edit profile cross.svg'
-import doneLogo from './edit profile right.svg'
+import cancelIcon from "./images/edit profile cross.svg"
+import donelIcon from "./images/edit profile right.svg"
 
 function EditProfile() {
     return (
@@ -16,38 +16,36 @@ function EditProfile() {
                     <form class="row g-3 container">
                         <div class="col-md-6">
                             <label for="inputFirstName2" class="form-label, editProfileLable1">First Name</label>
-                            <input type="text" name='firstName'  class="form-control" id="inputFirstName2" placeholder='input first name' />
+                            <input type="text" name='firstName' class="form-control" id="inputFirstName2" placeholder='input first name' />
                         </div>
                         <div class="col-md-6">
                             <label for="inputLastName2" class="form-label, editProfileLable1">Last Name</label>
-                            <input type="text" name='lastName'  class="form-control" id="inputLastName2" placeholder='input last name'  />
+                            <input type="text" name='lastName' class="form-control" id="inputLastName2" placeholder='input last name' />
                         </div>
-                        <div class="col-12">
+                        <div class="col-md-6">
                             <label for="email2" class="form-label, editProfileLable1">Email</label>
-                            <input type="email"  class="form-control" id="email2" placeholder='input email' />
-                        </div>
-                        <div class="col-6">
-                            <label for="inputUserName2"  class="form-label, editProfileLable1">Username</label>
-                            <input type="text" name='userName'  class="form-control" id="inputUserName2" placeholder='input username'  />
+                            <input type="email" class="form-control" id="email2" placeholder='input email' />
                         </div>
                         <div class="col-md-6">
                             <label for="inputPhoneNumber2" class="form-label, editProfileLable1">Phone Number</label>
                             <input type="number" class="form-control" id="inputPhoneNumber2" placeholder='input phone number' />
                         </div>
-                        <div class="col-md-4">
-                            <label for="inputState" class="form-label">State</label>
-                            <select id="inputState" class="form-select">
-                                <option selected>Choose...</option>
-                                <option>...</option>
-                            </select>
+                        <div class="col-md-6">
+                            <label for="inputUserName2" class="form-label, editProfileLable1">Username</label>
+                            <input type="text" name='userName' class="form-control" id="inputUserName2" placeholder='input username' />
                         </div>
-                        
-                       
-                        <div class="col-6">
-                            <button class="btn btn-primary"><img src={cancelLogo} alt="cancel" /></button>
+                        <div class="col-md-6">
+                            <label for="email2" class="form-label, editProfileLable1">Country</label>
+                            <input type="text" class="form-control" id="country2" placeholder='input country name' />
                         </div>
-                        <div class="col-6">
-                            <button type="submit" class="btn btn-primary"><img src={doneLogo} alt="done" /></button>
+                    
+                        <div className='btnFlex1'>
+                            <div class="">
+                                <button class="btn btn-primary"><img src={cancelIcon} alt="cancel" /></button>
+                            </div>
+                            <div class="">
+                                <button type="submit" class="btn btn-primary"><img src={donelIcon} alt="done" /></button>
+                            </div>
                         </div>
                     </form>
                 </div>
