c0cc4a6b9357a42929effd84d6b3d72bc1b1f4b1
tezzv <106775682+tezzv@users.noreply.github.com>
2022-10-13 02:36:37 +0530

    comming soon

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -86,7 +86,7 @@ const Navbar = () => {
             >
               <NavLink
                 exact
-                to="/comingsoon"
+                to="/commingsoon"
                 activeClassName="active"
                 className="nav-links"
                 onClick={handleClick}
